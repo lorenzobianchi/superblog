a121ff7caeca6731bd0c41b56666c46995815948
lorenzobianchi <lorenzo.bianchi@inmagik.com>
2017-12-03 21:51:30 +0100

    image field for avatar, better card for post-list, colors  :art:

--- superblog/ui/urls.py
+++ superblog/ui/urls.py
@@ -1,4 +1,6 @@
 from django.conf.urls import url
+from django.conf.urls.static import static
+from django.conf import settings
 from .views import (
     HomeView, PostListView, PostDetailView
 )
@@ -7,4 +9,4 @@ urlpatterns = [
     url(r'^$', HomeView.as_view(), name="home"),
     url(r'^post-list$', PostListView.as_view(), name="post_list"),
     url(r'^post-list/(?P<pk>\d+)/detail$', PostDetailView.as_view(), name="post_detail"),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
